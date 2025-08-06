14c4ed6f9dc40a6e05c5f3282c2eae7c10b17dd2
Victor Onyango <fabrisvicky1@gmail.com>
2025-08-06 09:27:33 +0300

    Fix bug login

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ let currentYear = 2025;
 let age = currentYear - yearOfBirth;
 
 const loginContainerDiv = document.getElementById(`loginContainerDiv`);
+                                                                                
 
+const outPutText =`Welcome! ${fullName}, You are logged in with Email address ${email}.`;
 
 loginContainerDiv.textContent = outPutText;
                                                                                 
